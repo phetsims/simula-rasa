3a98f850cfe53c0f71a2514488ea866139620c84
Chris Malley <cmalley@pixelzoom.com>
2015-11-12 13:17:20 -0700

    blank line

--- js/simula-rasa-config.js
+++ js/simula-rasa-config.js
@@ -6,6 +6,7 @@
  * @author Your Name (Your Affiliation)
  */
 require.config( {
+
   // An array of dependencies to load. Useful when require is defined as a config object before require.js
   // is loaded, and you want to specify dependencies to load as soon as require() is defined.
   deps: [ 'simula-rasa-main' ],
