49973e5d944899d7a19bd39251a83b318572e064
pixelzoom <cmalley@pixelzoom.com>
2021-07-20 13:18:41 -0600

    rename simulaRasaColorProfile.js to simulaRasaColors.js, https://github.com/phetsims/scenery-phet/issues/515

---
+++ js/common/simulaRasaColors.js
@@ -0,0 +1,29 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Defines the colors for this sim.
+ *
+ * All simulations should have a Colors.js file, see https://github.com/phetsims/scenery-phet/issues/642.
+ *
+ * For static colors that are used in more than one place, add them here.
+ *
+ * For dynamic colors that can be controlled via colorProfileProperty.js, add instances of ProfileColorProperty here,
+ * each of which is required to have a default color. Note that dynamic colors can be edited by running the sim from
+ * phetmarks using the "Color Edit" mode.
+ *
+ * @author {{AUTHOR}}
+ */
+
+import ProfileColorProperty from '../../../scenery/js/util/ProfileColorProperty.js';
+import simulaRasa from '../simulaRasa.js';
+
+const simulaRasaColors = {
+
+  // Background color that for screens in this sim
+  screenBackgroundColorProperty: new ProfileColorProperty( 'background', {
+    default: 'white'
+  } )
+};
+
+simulaRasa.register( 'simulaRasaColors', simulaRasaColors );
+export default simulaRasaColors;
--- js/simula-rasa/SimulaRasaScreen.js
+++ js/simula-rasa/SimulaRasaScreen.js
@@ -5,7 +5,7 @@
  */
 
 import Screen from '../../../joist/js/Screen.js';
-import simulaRasaColorProfile from '../common/simulaRasaColorProfile.js';
+import simulaRasaColors from '../common/simulaRasaColors.js';
 import simulaRasa from '../simulaRasa.js';
 import SimulaRasaModel from './model/SimulaRasaModel.js';
 import SimulaRasaScreenView from './view/SimulaRasaScreenView.js';
@@ -19,7 +19,7 @@ class SimulaRasaScreen extends Screen {
 
     const options = {
       //TODO if you include homeScreenIcon or navigationBarIcon, use JOIST/ScreenIcon
-      backgroundColorProperty: simulaRasaColorProfile.screenBackgroundColorProperty,
+      backgroundColorProperty: simulaRasaColors.screenBackgroundColorProperty,
       tandem: tandem
     };
 
