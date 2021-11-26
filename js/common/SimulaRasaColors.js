8e287c46b428abcbce087730b0d9f192e4d26774
Jonathan Olson <jonathan.olson@colorado.edu>
2021-11-25 19:51:19 -0700

    Initial common code typescript support, including Scenery import changes to prevent out-of-order load errors

--- js/common/SimulaRasaColors.js
+++ js/common/SimulaRasaColors.js
@@ -14,7 +14,7 @@
  * @author {{AUTHOR}}
  */
 
-import ProfileColorProperty from '../../../scenery/js/util/ProfileColorProperty.js';
+import { ProfileColorProperty } from '../../../scenery/js/imports.js';
 import simulaRasa from '../simulaRasa.js';
 
 const simulaRasaColors = {
