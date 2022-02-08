4c0d9992c9fd6b801c5be0b68c366a8b5dd477df
pixelzoom <cmalley@pixelzoom.com>
2022-02-08 10:13:13 -0700

    add SimulaRasaQueryParameters.js, https://github.com/phetsims/simula-rasa/issues/22

---
+++ js/common/SimulaRasaQueryParameters.js
@@ -0,0 +1,29 @@
+// Copyright 2019-2021, University of Colorado Boulder
+
+/**
+ * Defines query parameters that are specific to this simulation.
+ * Run with ?log to print query parameters and their values to the browser console at startup.
+ *
+ * @author {{AUTHOR}}
+ */
+
+import logGlobal from '../../../phet-core/js/logGlobal.js';
+import simulaRasa from '../simulaRasa.js';
+
+const SCHEMA_MAP = {
+  //TODO add schemas for query parameters
+};
+
+const SimulaRasaQueryParameters = QueryStringMachine.getAll( SCHEMA_MAP );
+
+// The schema map is a read-only part of the public API, in case schema details (e.g. validValues) are needed elsewhere.
+SimulaRasaQueryParameters.SCHEMA_MAP = SCHEMA_MAP;
+
+simulaRasa.register( 'SimulaRasaQueryParameters', SimulaRasaQueryParameters );
+
+// Log query parameters
+logGlobal( 'phet.chipper.queryParameters' );
+logGlobal( 'phet.preloads.phetio.queryParameters' );
+logGlobal( 'phet.naturalSelection.SimulaRasaQueryParameters' );
+
+export default SimulaRasaQueryParameters;
